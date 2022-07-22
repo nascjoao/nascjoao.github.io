4b68d60fa87378cff6d5eb703c563ecbc28feaaa
nascjoao <oi@joaonasc.dev>
2022-07-22 08:17:57 -0300

    feat: allow future images and images from github

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@ module.exports = {
   i18n: {
     locales: ["pt"],
     defaultLocale: "pt"
+  },
+  images: {
+    domains: ['www.github.com']
+  },
+  experimental: {
+    images: {
+      allowFutureImage: true
+    }
   }
 }
