3e08da4abba1602a8737114556299cd236815db4
nascjoao <oi@joaonasc.dev>
2022-11-11 20:15:20 -0300

    feat: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,12 @@
 import dayjs from 'dayjs'
+import { FaEnvelope, FaGithub, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
 import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <section>
+        <section className={styles.brandColumn}>
           <header>
             <img src="https://github.com/nascjoao.png" alt="" aria-hidden="true" />
             <h1>Nasc<span>.</span></h1>
@@ -14,12 +15,52 @@ export default function Footer() {
             <h2>Façam tudo com amor.</h2>
             <p>1 Coríntios 16:14</p>
           </div>
-          <p>João Victor Soares do Nascimento • {dayjs().year()}</p>
         </section>
+        <p className={styles.name}>João Victor Soares do Nascimento • {dayjs().year()}</p>
         <section className={styles.findMe}>
           <h1>onde me encontrar</h1>
-          <a href="https://github.com/nascjoao" target="_blank">GitHub</a>
-          <a href="https://linkedin.com/in/nascjoao" target="_blank">LinkedIn</a>
+          <div className={styles.socialList}>
+            <a href="https://github.com/nascjoao" target="_blank">
+              <span className={styles.icon}>
+                <FaGithub />
+              </span>
+              <span>
+                GitHub
+              </span>
+            </a>
+            <a href="https://linkedin.com/in/nascjoao" target="_blank">
+              <span className={styles.icon}>
+                <FaLinkedin />
+              </span>
+              <span>
+                LinkedIn
+              </span>
+            </a>
+            <a href="https://youtube.com/nascjoao" target="_blank">
+              <span className={styles.icon}>
+                <FaYoutube />
+              </span>
+              <span>
+                YouTube
+              </span>
+            </a>
+            <a href="https://instagram.com/nasc.dev" target="_blank">
+              <span className={styles.icon}>
+                <FaInstagram />
+              </span>
+              <span>
+                Instagram
+              </span>
+            </a>
+            <a href="mailto:oi@joaonasc.dev">
+              <span className={styles.icon}>
+                <FaEnvelope />
+              </span>
+              <span>
+                oi@joaonasc.dev
+              </span>
+            </a>
+          </div>
         </section>
       </div>
     </footer>
