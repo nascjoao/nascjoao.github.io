6947cc218320bc90edaa35866894106ac3a1d1fd
nascjoao <oi@joaonasc.dev>
2022-12-01 14:12:38 -0300

    refactor: abbreviate youtube

--- src/services/getYouTubeRedirects.js
+++ src/services/getYouTubeRedirects.js
@@ -2,7 +2,7 @@ module.exports = async function getYouTubeRedirects() {
   const fetchVideos = await fetch('https://nasc.dev/api/youtube');
   const videosData = await fetchVideos.json();
   const videosRedirects = videosData.data.map((video) => ({
-    source: `/youtube/${video.id}`,
+    source: `/yt/${video.id}`,
     destination: `https://www.youtube.com/watch?v=${video.id}`,
     permanent: true,
   }))
