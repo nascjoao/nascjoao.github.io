f066b90c102d0bed1b98f46ac24d8a53db8104e8
nascjoao <oi@nasc.dev>
2025-04-09 22:25:24 -0300

    feat(home): update header layout and add version link to GitHub releases

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,11 +1,19 @@
 import { getTranslations } from "next-intl/server";
 import LanguageSelect from "./language-select";
+import { AUTHOR_GITHUB, REPO_NAME, VERSION } from "@/shared/config";
 
 export default async function Home() {
   const t = await getTranslations();
   return (
     <>
-      <header className="flex justify-end items-center p-4 text-white">
+      <header className="flex justify-between items-center p-4 text-white">
+        <a
+          href={`https://github.com/${AUTHOR_GITHUB}/${REPO_NAME}/releases/tag/v${VERSION}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {VERSION}
+        </a>
         <LanguageSelect />
       </header>
       <main className="mx-auto max-w-xl px-4 py-8">
