c6d4ad9d0eea9d327691becf653abf1654ba6f89
nascjoao <oi@nasc.dev>
2025-04-09 22:24:59 -0300

    feat(config): add website configuration constants

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,1 +1,2 @@
 export { COOKIE_NAME, defaultLocale } from "./locale";
+export { AUTHOR_GITHUB, REPO_NAME, VERSION } from "./website";
---
+++ src/shared/config/website.ts
@@ -0,0 +1,3 @@
+export const AUTHOR_GITHUB = "nascjoao";
+export const REPO_NAME = "nascjoao.github.io";
+export const VERSION = process.env.NEXT_PUBLIC_WEBSITE_VERSION;
