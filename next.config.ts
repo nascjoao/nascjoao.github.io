b12f975cacb2133d3e55502d075d5c8090ab850c
nascjoao <oi@nasc.dev>
2025-04-11 20:22:07 -0300

    feat(svg): configure webpack for SVG handling and add TypeScript definitions

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,27 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_WEBSITE_VERSION: version,
   },
+  webpack(config) {
+    // @ts-expect-error - nextjs/typescript#nextConfig.webpack
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      },
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
 };
 
 const withNextIntl = createNextIntlPlugin("src/shared/i18n");
